8ccc5e082be3d7a296fa22189504208f25d89577
Bryant Wiggins <topwigit@gmail.com>
2023-08-28 14:40:35 -0400

    done

--- app.py
+++ app.py
@@ -10,7 +10,36 @@ results = soup.find(id='ResultsContainer')
 
 job_elements = results.find_all("div", class_="card-content") #This gives back an iterable set of data
 
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+)
 
+python_job_elements = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+for job_element in job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+    print()
+
+
+for job_element in python_job_elements:
+    title_element = job_element.find("h2", class_="title")
+    company_element = job_element.find("h3", class_="company")
+    location_element = job_element.find("p", class_="location")
+    links = job_element.find_all("a")
+    for link in links:
+        link_url = link["href"]
+        print(f"Apply here: {link_url}\n")
+
+
+
+# python_jobs = results.find_all("h2", string="Python") #Allows you to find the specifc string with limitations. IT look sfor the EXACT string (whitespace, spelling, case will effect the results)
 
 # print(results.prettify)
 
@@ -24,11 +53,4 @@ job_elements = results.find_all("div", class_="card-content") #This gives back a
 #     print()
     # print(job_element, end="\n"*4)
 
-for job_element in job_elements:
-    title_element = job_element.find("h2", class_="title")
-    company_element = job_element.find("h3", class_="company")
-    location_element = job_element.find("p", class_="location")
-    print(title_element.text.strip())
-    print(company_element.text.strip())
-    print(location_element.text.strip())
-    print()
+
