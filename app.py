6eb7563aec71acccac5d014f95913cdbf861899b
Bryant Wiggins <topwigit@gmail.com>
2023-08-28 13:53:27 -0400

    grabs everything from results container and gets iterable from card-content

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://realpython.github.io/fake-jobs/'
 page = requests.get(URL)
 
-print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id='ResultsContainer')
+
+job_elements = results.find_all("div", class_="card-content") #This gives back an iterable set of data
+
+# print(results.prettify)
+
+for job_element in job_elements:
+    print(job_element, end="\n"*4)
+
